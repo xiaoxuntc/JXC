a751fd5fee42672f32b08f970273d2cb1630feb5
sean <18399104@qq.com>
2024-10-15 17:58:19 +0800

    fouth edition main页面

--- src/main/java/com/java1234/controller/UserController.java
+++ src/main/java/com/java1234/controller/UserController.java
@@ -5,12 +5,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -105,5 +105,20 @@ public class UserController {
 		session.setAttribute("currentRole", currentRole);
 		map.put("success", true);
 		return map;
+	}
+
+	/**
+	 * 加载当前用户信息
+	 * 
+	 * @param session
+	 * @return
+	 * @throws Exception
+	 */
+	@ResponseBody
+	@GetMapping("/loadUserInfo")
+	public String loadUserInfo(HttpSession session) throws Exception {
+		User currentUser = (User) session.getAttribute("currentUser");
+		Role currentRole = (Role) session.getAttribute("currentRole");
+		return "欢迎您：" + currentUser.getTrueName() + "&nbsp;[&nbsp;" + currentRole.getName() + "&nbsp;]";
 	}
 }
